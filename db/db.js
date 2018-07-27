85078741b3092d2ec2c368cd7538dc4f73fa369a
Michelangelo Alesi <michelangelo@MacBook-Pro-di-Michelangelo.local>
2018-07-27 13:23:01 +0200

    new functions

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/artoo';
 
 
-function createDb(url){
+function createDb(){
 	MongoClient.connect(url, function(err, db) {
   		if (err) throw err;
   		console.log("Database created!");
@@ -14,10 +14,17 @@ function createDb(url){
 
 createDb(url);
 
-/*
+function insertObject(collection,obj){
+	MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
 
-function insertObject(id){
-	var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection(collection).insertOne(obj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    console.log(JSON.stringify(res));
+    db.close();
+  });
+}); 
 
 }
 
@@ -31,14 +38,14 @@ function updateObject(id){
 
 }
 
-function getObject(collection,id, callback){
+function getObject(collection,id){
 	var query = {_id: id};
 	dbo.collection(collection).find(query).toArray(function(err, result) {
     db.close();
-    callback(err, result);
+
   });
 
 
 }
 
-*/
+
