51305bb692dd076e36bef6d66036f0de04d4de22
Michelangelo Alesi <michelangelo@MacBook-Pro-di-Michelangelo.local>
2018-07-27 13:04:58 +0200

    new add

---
+++ db/db.js
@@ -0,0 +1,41 @@
+var mongo = require('mongodb');
+	
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/artoo';
+
+
+function createDb(url){
+	MongoClient.connect(url, function(err, db) {
+  		if (err) throw err;
+  		console.log("Database created!");
+  		db.close();
+	});
+}
+/*
+
+function insertObject(id){
+	var myobj = { name: "Company Inc", address: "Highway 37" };
+
+}
+
+function deleteObject(id){
+
+
+}
+
+function updateObject(id){
+
+
+}
+
+function getObject(collection,id, callback){
+	var query = {_id: id};
+	dbo.collection(collection).find(query).toArray(function(err, result) {
+    db.close();
+    callback(err, result);
+  });
+
+
+}
+
+*/
